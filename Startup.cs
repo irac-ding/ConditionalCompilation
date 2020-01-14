65ded382819478a6e9cfec93388debae10a6a77b
irac <iracding@tvunetworks.com>
2020-01-14 11:39:42 +0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿#define NotSQLite
+//#define NotSQLite
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
